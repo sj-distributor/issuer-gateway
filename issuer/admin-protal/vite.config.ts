77edd9d0f07ba6a8da5af457c05292b47b635c1c
COLIN.H <battle_net_incn@163.com>
2023-10-18 10:48:11 +0800

    enhance build
    enhance build

--- issuer/admin-protal/vite.config.ts
+++ issuer/admin-protal/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite"
 import { resolve } from "path"
 import react from "@vitejs/plugin-react"
 import checker from "vite-plugin-checker"
+import { dts } from "rollup-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
+      jsxRuntime: "automatic",
       babel: {
         plugins: ["@emotion/babel-plugin"],
       },
@@ -19,6 +21,11 @@ export default defineConfig({
       typescript: true,
     }),
   ],
+  build: {
+    rollupOptions: {
+      plugins: [dts()],
+    },
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
